4cd17895ea90ba546f2710980ad67782033b163c
Ankit kumar <ankit.kumar.info22@gmail.com>
2024-01-15 12:03:08 +0530

    fixed ticket already purchased toast

--- src/features/Bookings/useBookTicket.ts
+++ src/features/Bookings/useBookTicket.ts
@@ -1,45 +1,50 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { IRegisterUserObject, bookTicket } from "@/services/apiBooking";
 import { useMutation } from "react-query";
 import { useToast } from "@/components/ui/use-toast";
 
 const useBookTicket = () => {
-	const { toast } = useToast();
-	const { isLoading, mutate: bookTicketNow } = useMutation({
-		mutationFn: (reqObj: IRegisterUserObject) => bookTicket(reqObj),
-		onSuccess: (resData) => {
-			if (resData && resData.status !== 500) {
-				// Assuming resData is the full URL
-				window.location.href = resData;
-			} else {
-				return resData;
-			}
-		},
-		onError: (error: any) => {
-			// Handle error if needed
-			if (error?.response?.data?.message === "TICKET_SOLD_OUT") {
-				toast({
-					variant: "error",
-					title: "Ticket purchase failed",
-					description: "Tickets have sold out.",
-				});
-			} else if (error?.response?.data?.message === "TICKET_ALREADY_PURCHASED") {
-				toast({
-					variant: "error",
-					title: "Ticket Purchase",
-					description: "You have already purchased this ticket.",
-				});
-			} else {
-				// Handle other errors
-				toast({
-					variant: "error",
-					title: "Error",
-					description: "An error occurred while purchasing the ticket.",
-				});
-			}
-		},
-	});
+  const { toast } = useToast();
+  const { isLoading, mutate: bookTicketNow } = useMutation({
+    mutationFn: (reqObj: IRegisterUserObject) => bookTicket(reqObj),
+    onSuccess: (resData) => {
+      if (resData && resData.status === 200) {
+        // Assuming resData is the full URL
+        window.location.href = resData;
+      } else if (
+        resData &&
+        resData.status === 300 &&
+        resData.message === "TICKET_SOLD_OUT"
+      ) {
+        toast({
+          variant: "error",
+          title: "Ticket purchase failed",
+          description: "Tickets have sold out.",
+        });
+      } else if (
+        resData &&
+        resData.status === 300 &&
+        resData.message === "TICKET_ALREADY_PURCHASED"
+      ) {
+        toast({
+          variant: "error",
+          title: "Ticket Purchase",
+          description: "You have already purchased this ticket.",
+        });
+      }
+    },
+    onError: (error) => {
+      // Handle error if needed
 
-	return { isLoading, bookTicketNow };
+      toast({
+        variant: "error",
+        title: "Error",
+        description: `An error occurred while purchasing the ticket. ${error}`,
+      });
+    },
+  });
+
+  return { isLoading, bookTicketNow };
 };
 
 export default useBookTicket;
